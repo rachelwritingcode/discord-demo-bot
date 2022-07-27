7d4ec7463ba713d81a06df6bb3d0ef056815a2f8
Rachel Wang <21071004+rachelwritingcode@users.noreply.github.com>
2022-07-26 22:23:26 -0400

    Add additional bot reactions

--- scripts/bot.py
+++ scripts/bot.py
@@ -7,6 +7,7 @@ import os
 # IMPORT LOAD_DOTENV FUNCTION FROM DOTENV MODULE.
 from dotenv import load_dotenv
 
+
 # LOADS THE .ENV FILE THAT RESIDES ON THE SAME LEVEL AS THE SCRIPT.
 load_dotenv()
 
@@ -16,23 +17,32 @@ DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 # GETS THE CLIENT OBJECT FROM DISCORD.PY. CLIENT IS SYNONYMOUS WITH BOT.
 bot = discord.Client()
 
-# EVENT LISTENER FOR WHEN A NEW MESSAGE IS SENT TO A CHANNEL.
-@bot.event
-async def on_message(message):
-	# CHECKS IF THE MESSAGE THAT WAS SENT IS EQUAL TO "HELLO".
-	if message.content == "hello":
-		# SENDS BACK A MESSAGE TO THE CHANNEL.
-		await message.channel.send("hello there jedi!")
-
-
 
 # EVENT LISTENER FOR WHEN A NEW MESSAGE IS SENT TO A CHANNEL.
 @bot.event
 async def on_message(message):
 	# CHECKS IF THE MESSAGE THAT WAS SENT IS EQUAL TO "HELLO".
-	if message.content == "bonjour":
+
+	message_text = message.content.lower()
+	
+	if message_text.find("hello") != -1:
 		# SENDS BACK A MESSAGE TO THE CHANNEL.
-		await message.channel.send("bonjour jedi!")
+		await message.channel.send("Hello jedi! May the force be with you!")
+		return
+	elif message_text.find("bonjour") != -1:
+		# SENDS BACK A MESSAGE TO THE CHANNEL.
+		await message.channel.send("Bonjour jedi! Que le force soit avec toi!")
+		return
+	elif message_text.find("grogu") != -1:
+		# SENDS BACK A MESSAGE TO THE CHANNEL.
+		await message.channel.send("Where's Mando?")
+		return
+	elif message_text.find("mando") != -1:
+		# SENDS BACK A MESSAGE TO THE CHANNEL.
+		await message.channel.send("This is the way!")
+		return
+
+
 
 
 
